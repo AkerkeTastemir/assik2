5056e599f31dd0f16807857961a8df68ffc644b5
Akerke <akerketastemirova@icloud.com>
2025-04-11 19:58:37 +0500

    MyArayList.java class

---
+++ src/MyArrayList.java
@@ -0,0 +1,194 @@
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class MyArrayList<T> implements MyList<T> {
+
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public MyArrayList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity > elements.length) {
+            int newCapacity = Math.max(elements.length * 2, minCapacity);
+            elements = Arrays.copyOf(elements, newCapacity);
+        }
+    }
+
+    @Override
+    public void add(T item) {
+        ensureCapacity(size + 1);
+        elements[size++] = item;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        rangeCheck(index);
+        elements[index] = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        rangeCheckForAdd(index);
+        ensureCapacity(size + 1);
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = item;
+        size++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(item);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public T get(int index) {
+        rangeCheck(index);
+        return (T) elements[index];
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public T getFirst() {
+        return get(0);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public T getLast() {
+        return get(size - 1);
+    }
+
+    @Override
+    public void remove(int index) {
+        rangeCheck(index);
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elements, index + 1, elements, index, numMoved);
+        }
+        elements[--size] = null;
+    }
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    @Override
+    public void sort() {
+        Arrays.sort(elements, 0, size);
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (object.equals(elements[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (object.equals(elements[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) >= 0;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(elements, size);
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private void rangeCheck(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    private void rangeCheckForAdd(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size;
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public T next() {
+                return (T) elements[currentIndex++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("Remove not supported");
+            }
+        };
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        for (int i = 0; i < size; i++) {
+            @SuppressWarnings("unchecked")
+            T element = (T) elements[i];
+            action.accept(element);
+        }
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        @SuppressWarnings("unchecked")
+        T[] array = (T[]) Arrays.copyOf(elements, size);
+        return Arrays.spliterator(array);
+    }
+
+
+}
