482d2bf4ae45d25a403eea275be35aa0487ddde9
Akerke <akerketastemirova@icloud.com>
2025-04-13 22:05:07 +0500

    Main.java added

---
+++ src/start/Main.java
@@ -0,0 +1,10 @@
+package start;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Здесь будет основная логика программы, пока ничего не выполняется
+        System.out.println("Программа запущена.");
+    }
+
+}
